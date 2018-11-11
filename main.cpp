dfb53dfadfe3b4a8f9dfa3c839828029d7b7aa9a
Chris <chrisjohnsonmail@gmail.com>
2018-11-10 22:04:47 -0600

    improvement:  submodules build, main compiles, cannot find libaries due to build system looking for release name instead of debug.  Still WIP

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
-#include "arkClient.h"
 #include "arkCrypto.h"
+
+#include "arkClient.h"
 
 int main(int argc, char* argv[]) {
 	std::cout << "Welcome onboard the ARK!" << std::endl;
